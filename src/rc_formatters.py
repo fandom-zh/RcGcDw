c37fd141cbb9a1a49811d8dec22c7f8fc822dd5c
MarkusRost <sukramxro@gmail.com>
2020-08-01 21:23:15 +0000

    remove ugly work around

--- src/rc_formatters.py
+++ src/rc_formatters.py
@@ -25,7 +25,6 @@ def compact_formatter(action, change, parsed_comment, categories, recent_changes
 		author_url = link_formatter(create_article_path("User:{user}".format(user=change["user"])))
 		author = change["user"]
 	parsed_comment = "" if parsed_comment is None else " *("+parsed_comment+")*"
-	parsed_comment = re.sub(r"([^<]|\A)(http(s)://.*?)( |\Z)", "\\1<\\2>\\4", parsed_comment)  # see #97
 	if action in ["edit", "new"]:
 		edit_link = link_formatter("{wiki}index.php?title={article}&curid={pageid}&diff={diff}&oldid={oldrev}".format(
 			wiki=WIKI_SCRIPT_PATH, pageid=change["pageid"], diff=change["revid"], oldrev=change["old_revid"],
