99a1aa782796f7c33f414bf7bd664c1172534a6f
Frisk <furrtrex43@protonmail.ch>
2020-11-08 01:36:41 +0100

    Added debug messages

--- src/message_redaction.py
+++ src/message_redaction.py
@@ -28,26 +28,31 @@ def create_connection() -> (sqlite3.Connection, sqlite3.Cursor):
 	_db_connection = sqlite3.connect(settings['auto_suppression'].get("db_location", ':memory:'))
 	_db_connection.row_factory = sqlite3.Row
 	_db_cursor = db_connection.cursor()
+	logger.debug("Database connection created")
 	return _db_connection, _db_cursor
 
 
 def check_tables():
 	rep = db_cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='messages';")
 	if not rep.fetchone():
+		logger.debug("No schema detected, creating schema!")
 		create_schema()
 
 
 def add_entry(pageid: int, revid: int, logid: int, message):
 	db_cursor.execute("INSERT INTO messages (message_id, content) VALUES (?, ?)", (message.get("message_id"), message))
 	db_cursor.execute("INSERT INTO event (pageid, revid, logid, msg_id) VALUES (?, ?, ?, ?)", (pageid, revid, logid, message.get("message_id")))
+	logger.debug("Adding an entry to the database (pageid: {}, revid: {}, logid: {}, message: {})".format(pageid, revid, logid, message))
 
 
 def delete_messages(pageid: int):
 	to_delete = db_cursor.execute("SELECT msg_id FROM event WHERE pageid = ?", (pageid))
 	msg_to_remove = []
+	logger.debug("Deleting messages for pageid: {}".format(pageid))
 	for message in to_delete:
 		webhook_url = "{main_webhook}/messages/{message_id}".format(main_webhook=settings["webhookURL"], message_id=message[0])
 		msg_to_remove.append(message[0])
+		logger.debug("Removing following message: {}".format(message))
 		send_to_discord(None, DiscordMessageMetadata("DELETE", webhook_url=webhook_url))
 	db_cursor.executemany("DELETE FROM messages WHERE message_id = ?", msg_to_remove)
 
