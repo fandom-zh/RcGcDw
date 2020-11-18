1c86886a2b16be49050c839fa0eaff1cc121addb
Frisk <furrtrex43@protonmail.ch>
2020-11-18 10:05:40 +0100

    Translation update, fix #164

--- scripts/generate-translations.sh
+++ scripts/generate-translations.sh
@@ -1,5 +1,5 @@
 cd ..
-declare -a StringArray=("discussion_formatters" "rc_formatters" "rcgcdw" "rc" "misc", "redaction")
+declare -a StringArray=("discussion_formatters" "rc_formatters" "rcgcdw" "rc" "misc")
 for file in ${StringArray[@]}; do
   xgettext -L Python --package-name=RcGcDw -o "locale/templates/$file.pot" src/$file.py
 done
@@ -8,4 +8,10 @@ do
   for file in ${StringArray[@]}; do
     msgmerge -U locale/$language/LC_MESSAGES/$file.po locale/templates/$file.pot
   done
+done
+# Exceptions
+xgettext -L Python --package-name=RcGcDw -o "locale/templates/redaction.pot" src/discord/redaction.py
+for language in de fr lol pl pt-br ru uk zh_Hans zh_Hant
+do
+  msgmerge -U locale/$language/LC_MESSAGES/redaction.po locale/templates/redaction.pot
 done
