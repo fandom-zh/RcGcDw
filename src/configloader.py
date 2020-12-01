3fb6a04363710789c3fab5acfe1da12be79f506e
Frisk <furrtrex43@protonmail.ch>
2020-12-01 12:50:22 +0100

    Bump version number

--- src/configloader.py
+++ src/configloader.py
@@ -6,7 +6,7 @@ try:  # load settings
 		if settings["limitrefetch"] < settings["limit"] and settings["limitrefetch"] != -1:
 			settings["limitrefetch"] = settings["limit"]
 		if "user-agent" in settings["header"]:
-			settings["header"]["user-agent"] = settings["header"]["user-agent"].format(version="1.13")  # set the version in the useragent
+			settings["header"]["user-agent"] = settings["header"]["user-agent"].format(version="1.13.1")  # set the version in the useragent
 except FileNotFoundError:
 	logging.critical("No config file could be found. Please make sure settings.json is in the directory.")
 	sys.exit(1)
