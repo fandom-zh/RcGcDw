1ad8528f79ddff291ee4eb1b26d6b285cd586193
Frisk <702385-piotrex43@users.noreply.gitlab.com>
2021-04-20 13:11:43 +0000

    Bump version because I apparently messed up

--- src/configloader.py
+++ src/configloader.py
@@ -13,7 +13,7 @@ def load_settings():
 			if settings["limitrefetch"] < settings["limit"] and settings["limitrefetch"] != -1:
 				settings["limitrefetch"] = settings["limit"]
 			if "user-agent" in settings["header"]:
-				settings["header"]["user-agent"] = settings["header"]["user-agent"].format(version="1.13.1")  # set the version in the useragent
+				settings["header"]["user-agent"] = settings["header"]["user-agent"].format(version="1.13.1.2")  # set the version in the useragent
 	except FileNotFoundError:
 		logging.critical("No config file could be found. Please make sure settings.json is in the directory.")
 		sys.exit(1)
