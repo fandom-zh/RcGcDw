04904aebda3d87ec57429f27aeb987e84d4ca477
Frisk <furrtrex43@protonmail.ch>
2021-05-15 13:03:30 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='RcGcDw',
-	version='1.12',
+	version='1.14',
 	packages=[''],
 	url='https://gitlab.com/piotrex43/RcGcDw/',
 	license='GNU GPLv3',
@@ -12,5 +12,5 @@ setup(
 	keywords=['MediaWiki', 'recent changes', 'Discord', 'webhook'],
 	package_dir={"": "src"},
 	install_requires=["beautifulsoup4 >= 4.6.0", "requests >= 2.18.4", "schedule >=  0.5.0", "lxml >= 4.2.1"],
-	python_requires="3.6"
+	python_requires="3.7"
 )
